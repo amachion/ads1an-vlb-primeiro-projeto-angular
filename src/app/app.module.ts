e648432fffd6ef35ddf4a2f4c5ebec09a9774629
Andreia <andreia.machion@saojudas.br>
2021-10-01 08:28:42 -0300

    projeto ola angular completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { OlaAngularComponent } from './ola-angular/ola-angular.component';
 
 import { AppComponent } from './app.component';
+import { OlaAngularCliComponent } from './ola-angular-cli/ola-angular-cli.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OlaAngularComponent,
+    OlaAngularCliComponent
   ],
   imports: [
     BrowserModule
